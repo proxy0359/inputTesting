780f5c32431cc6eaec5d86b19ead8f2452f849d7
proxyjem jackprox <jemjem.santos03@gmail.com>
2022-12-10 01:31:35 +0800

    input done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Input from "./Components/input";
+import Input from "./Components/Input";
 
 function App() {
   return (
--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Input.module.css";
 
 const Input = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [inputIsValid, setInputIsValid] = useState(false);
+  const [inputIsTouched, setInputIsTouched] = useState(false);
+  const inputOnChangeHandler = (event) => {
+    setInputValue(event.target.value);
+    if (event.target.value.trim() === "") {
+      setInputIsValid(false);
+      return;
+    }
+    setInputIsValid(true );
+  };
+  const onBlurHandler = () => {
+    setInputIsTouched(true);
+    if (inputValue.trim() === "") {
+      setInputIsValid(false);
+      return;
+    }
+    setInputIsValid(true);
+    setInputIsTouched(true);
+  };
   const inputSubmitHandler = (event) => {
     event.preventDefault();
+    if (inputValue.trim() === "") {
+      setInputIsValid(false);
+      return;
+    }
+    setInputIsValid(true);
+    setInputValue("");
+    console.log(inputValue);
   };
+  const errorHandler = !inputIsValid && inputIsTouched ? style.error : "";
   return (
     <form onSubmit={inputSubmitHandler}>
       <label htmlFor="">Username</label>
-      <input type="text" />
+      <input
+        type="text"
+        onChange={inputOnChangeHandler}
+        value={inputValue}
+        onBlur={onBlurHandler}
+      />
+      {errorHandler && (
+        <p className={errorHandler}>Please enter a valid input</p>
+      )}
       <button type="submit">Add</button>
     </form>
   );
 };
 
-export default input;
+export default Input;
